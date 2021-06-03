50b92425b4b167e2fe985921a631c058bc822317
Timaaos <75538611+timaaos@users.noreply.github.com>
2021-06-03 18:02:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "mc-server-setup=mc-server-setup.setupserver:main",
+            "mc-server-setup=mcsetupserver:main",
         ]
     },
 )
