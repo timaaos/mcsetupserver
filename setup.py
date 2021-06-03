a2cc9bee436160916fe94fc0e0050b54f86c5ee2
Timaaos <75538611+timaaos@users.noreply.github.com>
2021-06-03 18:11:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "minecraft-ez-server=minecraft-es-server:main",
+            "mc_ez_server=minecraft-ez-server:serversetup",
         ]
     },
 )
